097359982dff791362d0bec57c61964c02faa5ad
sideffect0 <mail3renjith@gmail.com>
2016-10-07 11:50:53 +0530

    correct flag parse & argument options

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "net/http"
 import "flag"
+import "fmt"
 
 func main() {
     
-    bind := flag.String("b", ":5656", "Server host:port to listen")
-    panic(http.ListenAndServe(*bind, http.FileServer(http.Dir("."))))
+    host := flag.String("host", "localhost", "Server host to listen")
+    port := flag.String("port", "5656", "Server port")
+    dir  := flag.String("dir", ".", "Directory location to files")
+    flag.Parse()
+    bind := fmt.Sprintf("%s:%s", *host, *port)
+    fmt.Println("Listening at ", bind)
+    panic(http.ListenAndServe(bind, http.FileServer(http.Dir(*dir))))
 
 }
