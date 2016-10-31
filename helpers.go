e306efad1cbbc46cb0b9f236655d4b5dc07349e1
sideffect0 <mail3renjith@gmail.com>
2016-10-31 10:19:25 +0530

    add checking for available bind

--- helpers.go
+++ helpers.go
@@ -4,17 +4,33 @@ import "net"
 import "strings"
 
 func GetAddrs() (addrs []string, err error) {
- addr_list, err := net.InterfaceAddrs()
- 
- if err != nil{
-    return nil, err
- }
- for _, addr := range(addr_list){
-   split := strings.Split(addr.String(), "/")
-   // removing ipv6 for now
-   if net.ParseIP(split[0]).To4() != nil {
-    addrs = append(addrs, split[0])
-   }
- }
- return
+	addr_list, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return nil, err
+	}
+	for _, addr := range addr_list {
+		split := strings.Split(addr.String(), "/")
+		// removing ipv6 for now
+		if net.ParseIP(split[0]).To4() != nil {
+			addrs = append(addrs, split[0])
+		}
+	}
+	return
+}
+
+func GetFreeBind() (free_bind string) {
+	return
+}
+
+func BindAvailable(bind string) (status bool, err error) {
+
+	tester, err := net.Listen("tcp", host)
+	// port not available with given bind
+	if err != nil {
+		return false, err
+	}
+	// free up for dev-server
+	server.Close()
+	return true, nil
 }
--- main.go
+++ main.go
@@ -3,30 +3,42 @@ package main
 import "net"
 import "net/http"
 import "flag"
-import "fmt"
+import "log"
+
+func withAvailableHost(bind string, http_serve func(free_bind string)) {
+	if BindAvailable(bind) {
+		http_serve(bind)
+	} else if Confirm() {
+		// call with free ports
+	} else {
+		log.Fatalln("Exiting not available : ", bind)
+	}
+}
 
 func main() {
-    
-    host := flag.String("host", "0.0.0.0", "Server host to listen")
-    port := flag.String("port", "5656", "Server port")
-    dir  := flag.String("dir", ".", "Directory location to files")
-    flag.Parse()
-    // i love anon :)
-    greet:= func(addr string) {
-     fmt.Println("Listening at ", addr)
-    }
-    bind := net.JoinHostPort(*host, *port)
-    if *host != "0.0.0.0" {
-       greet(bind)
-    }else{
-      addr_list, err := GetAddrs()
-      if err != nil{
-        fmt.Println("error on getting system interface address")
-      }
-      for _, addr := range(addr_list) {
-         greet(net.JoinHostPort(addr, *port))
-      }
-    }
-    panic(http.ListenAndServe(bind, http.FileServer(http.Dir(*dir))))
+
+	host := flag.String("host", "0.0.0.0", "Server host to listen")
+	port := flag.String("port", "5656", "Server port")
+	dir := flag.String("dir", ".", "Directory location to files")
+	flag.Parse()
+	// i love anon :)
+	greet := func(addr string) {
+		log.Println("Listening at ", addr)
+	}
+	bind := net.JoinHostPort(*host, *port)
+	if *host != "0.0.0.0" {
+		greet(bind)
+	} else {
+		addr_list, err := GetAddrs()
+		if err != nil {
+			log.Println("error on getting system interface address")
+		}
+		for _, addr := range addr_list {
+			greet(net.JoinHostPort(addr, *port))
+		}
+	}
+	withAvailableHost(bind, func(bind string) {
+		log.Fatalln(http.ListenAndServe(bind, http.FileServer(http.Dir(*dir))))
+	})
 
 }
